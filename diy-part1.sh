c73a474136122edf10b70d6cae3de569103d6f48
hudi-ya <66246822+hudi-ya@users.noreply.github.com>
2021-12-14 21:40:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone -b LUCI-LUA-UCITRACK https://github.com/hudi-ya/feed-netkeeper openwrt/package/netkeeper
+#git clone -b LUCI-LUA-UCITRACK https://github.com/hudi-ya/feed-netkeeper openwrt/package/netkeeper
