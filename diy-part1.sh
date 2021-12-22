42dcb53399e314cc0b3fde4951c5a7c2ce7f01f7
hudi-ya <66246822+hudi-ya@users.noreply.github.com>
2021-12-22 09:32:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,6 +19,7 @@
 #git clone -b LUCI-LUA-UCITRACK https://github.com/hudi-ya/feed-netkeeper openwrt/package/netkeeper
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 git pull
